6bea68bd06fb92d7827032f80fd29dd5a4343d37
danfham <dan.cunningham91@icloud.com>
2023-11-04 10:21:09 -0400

    completed and tested results for activity

--- function_practice_part4.py
+++ function_practice_part4.py
@@ -16,5 +16,31 @@ def mult_list(inputArr):
     print(f"the result of multiplying each item in the array is: ", result)
     return result
 
+def rev_string(str):
+    result = str[::-1]
+    print(f"the original string is: ", str, " and reversed: ", result.capitalize())
+    return result
+
+
+def num_within(test,first,last):
+    if(test > first and test < last):
+        print(f'The test value ',test,' is within the range of', first, ' and ', last)
+        result = True
+    else: 
+        print(f'The test value ',test,' is not within the range of ', first, ' and ', last)
+        result = False
+    return result
+
+
+
+def pascal(val):
+    for item in range(val):
+        print(' '*(val-1), end='')
+        print(' '.join(map(str,str(11**item))))
+
 max_num([1,2,3])
 mult_list([1,2,3])
+rev_string("Duck")
+num_within(3,2,4)
+num_within(10,2,5)
+pascal(5)
